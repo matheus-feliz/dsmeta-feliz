746a548270db00bc25f087cfdf13c776bac86cee
sem-nome <matheusfoliveira172@gmail.com>
2022-07-14 15:13:15 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import icon from '../../assets/img/image1.svg'
+import'./styles.css'
+function Header() {
+    return (<>
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={icon} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por
+              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+            </p>
+        </div>
+    </header>
+    </>)
+}
+
+export default Header
