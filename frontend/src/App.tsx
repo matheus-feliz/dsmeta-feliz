96e2a709fa8cd6b1d9f4eefb481deaae852aa9b2
sem-nome <matheusfoliveira172@gmail.com>
2022-07-14 06:49:50 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
-  return(<h1>
-    Óla mundo
-  </h1>)
+  return(
+  <>
+  <h1>  Óla mundo  </h1>
+  <NotificationButton />
+  </>
+  )
 }
 
 export default App
